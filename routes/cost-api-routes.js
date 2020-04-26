3bc2a8ebcc3df14b2625d5c773ce142e633d79b1
NA Flipper <dioncleung@gmail.com>
2020-04-25 22:03:07 -0700

    added functionality to receive tasks and costs

--- routes/cost-api-routes.js
+++ routes/cost-api-routes.js
@@ -25,6 +25,8 @@ module.exports = function (app) {
             name: req.body.name,
             description: req.body.description,
             cost: req.body.cost,
+            purchased: req.body.purchased,
+            eventId: req.body.event_id
         }).then(dbCost => res.json(dbCost));
     });
 
